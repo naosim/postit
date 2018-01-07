62e3032f8168a036e642f9a4aa7fe5d232fa0283
naosim <fujitanao@fujitaMacBook-Pro.local>
2018-01-07 21:57:49 +0900

    ビルド環境を整理

--- build_and_main.js
+++ build_and_main.js
@@ -1,29 +1,77 @@
+// ビルド設定
 const jsFile = './docs/js/main.js';
 const buildScript = `tsc src/main.ts --outFile ${jsFile}`;
 
-const http = require('http');
+// 標準ライブラリの読み込み
 const fs = require('fs');
 const exec = require('child_process').exec;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+/**
+* サーバ作成
+*
+* action: (req, res) => void
+* option: {
+*   hostname:string,
+*   port:number,
+*   timeout:number,
+*   onCatchException: (e, req, res) => void,
+*   onTimeout: (req, res) => void
+* }
+*/
+function createServer(
+  action,
+  option
+) {
+  const domain = require('domain');
+  const hostname = (option && option.hostname) || '127.0.0.1';
+  const port = (option && option.port) || 3000;
+  const timeout = (option && option.timeout) || 30 * 1000;
+  const onCatchException = (option && option.onCatchException) || ((e, req, res) => {res.statusCode = 500; res.end(e);});
+  const onTimeout = (option && option.onTimeout) || ((req, res) => {res.statusCode = 408; res.end('');});
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/javascript');
+  var server = require('http').createServer((req, res)=> {
+    res.on('timeout', () => onTimeout(req, res));
 
-  exec(buildScript, (err, body) => {
-    if(err) {
-      console.log(err);
-      console.log(body);
+    var d = domain.create();
+    d.on('error', (e) => onCatchException(e, req, res));
+    d.run(() => action(req, res));
+  });
 
-      const message = body.split('\n').join('\\n').split("'").join("\\'");
-      res.end(`alert('${message}')`);
-    } else {
-      res.end(fs.readFileSync(jsFile, 'utf8'));
-    }
+  server.setTimeout(timeout);
+  server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
 
-  })
-});
+  process.on('uncaughtException', (e) => console.log(e));
+}
 
-server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
+function createBuildServer(
+  buildScript,
+  jsFile,
+  contentType, // 任意, デフォルト: 'text/javascript'
+  serverOption
+) {
+  contentType = contentType || 'text/javascript';
+  createServer(
+    (req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', contentType);
+
+      exec(buildScript, (err, body) => {
+        if(err) {// ビルド失敗
+          console.log(err);
+          console.log(body);
+
+          // エラーメッセージの取得 (改行等を適当にエスケープする)
+          const message = body.split('\n').join('\\n').split("'").join("\\'");
+          res.end(`alert('${message}')`);
+        } else {// ビルド成功
+          // 生成されたjsを返す
+          res.end(fs.readFileSync(jsFile, 'utf8'));
+        }
+      })
+    },
+    serverOption
+  );
+}
+
+// 実行
+createBuildServer(buildScript, jsFile);
